3e9f83db822dcc11938e421468f8942fa25aab0d
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-05-17 22:50:37 -0500

    feat: actualizar configuración de base de datos a PostgreSQL

--- sistema_bancario/settings.py
+++ sistema_bancario/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'sistema_bancario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_sistema_bancario',
+        'USER': 'postgres',
+        'PASSWORD': 'a322',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
